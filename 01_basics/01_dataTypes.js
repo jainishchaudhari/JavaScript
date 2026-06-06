78887f6f52db38642a56be60254ddcbbb9565b0e
Jainish <chaudharijainish9498@gmail.com>
2026-06-06 11:07:44 +0000

    dataType

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code newer version
+
+// alert(3+3); // we are using node js , not browser so alert is not defined
+
+console.log(3 + 3);
+
+// dataType
+
+let name = "jainish";
+let age = 3;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint 
+// string => "", ''
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned value
+// symbol
+
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
+
+//array
+
