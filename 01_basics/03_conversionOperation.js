7b94e067c6e0ebc53312326f9e50ab3a3823b5b4
Jainish <chaudharijainish9498@gmail.com>
2026-07-11 10:32:42 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(score);
+
+
+let valueConversion = Number(score);
+
+// console.log(typeof valueConversion);
+// console.log(typeof(valueConversion));
+// console.log(valueConversion);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1 ; falsh => 0
+//null => 0
+
+
+let isLoggedIn = "jainish";
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanisLoggedIn);
+
+
+//1 => true ; 0 => false
+//"" => false; 
+//"jainish" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
