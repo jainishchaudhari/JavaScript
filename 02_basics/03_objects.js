a5cead0098beb8229aed5d5798b98e9226cc8a94
Jainish <chaudharijainish9498@gmail.com>
2026-07-14 23:28:09 +0530

    objects function use

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -16,7 +16,27 @@ const jsUser = {
     lastLoggedInDays: [ "mon", "wed", "fri" ]
 };
 
-console.log(jsUser.age);
-console.log(jsUser["email"]);
+// console.log(jsUser.age);
+// console.log(jsUser["email"]);
+// console.log(jsUser["mySym"]);
 
-console.log(jsUser["mySym"]);
+// jsUser.email = "jainish@google.com";
+
+// Object.freeze(jsUser);
+
+jsUser.email = "jainish@google.com";
+// console.log(jsUser);
+
+
+jsUser.greeting1 = function () {
+    console.log("hello js"); 
+}
+
+jsUser.greeting2 = function () {
+    console.log(`hello js, ${this["f Name"]}`);   
+}
+
+
+console.log(jsUser.greeting1());
+console.log(jsUser.greeting2());
+
