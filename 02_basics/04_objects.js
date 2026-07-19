b0b026dbcaef332b2e535c2a316f5d334e0b87a7
Jainish <chaudharijainish9498@gmail.com>
2026-07-19 23:29:30 +0530

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -47,10 +47,43 @@ const user1 = [
 ];
 
 user1[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("isLogged"));
+// console.log(tinderUser.hasOwnProperty("isLogged"));
+
+
+
+const course = {
+    courseName: "JavaScript",
+    price: 999,
+    courseInstructor: "Jainish"
+};
+
+// course.courseInstructor
+
+const {courseInstructor: me} = course;
+
+// console.log(courseInstructor);
+
+console.log(me);   // easy way to call key value {Destructuring with Alias}
+
+// console.log(course.courseInstructor);
+// console.log(course["courseInstructor"]);
+
+//what is json?
+
+// {
+//     "name": "jainish",
+//     "coursename": "JavaScript",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+];
