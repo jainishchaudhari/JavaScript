ab63ddc7acff40bf06a636a1ed52b5c67ddb4f0a
Jainish <chaudharijainish9498@gmail.com>
2026-07-22 19:47:34 +0530

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,54 @@
+//if
+const isUserLoggedIn = true;
+
+const temp = 41;
+
+// if (temp < 50) {
+//   console.log("temp is less than 50");
+// } else {
+//   console.log("tem is greater than 50");
+// }
+// console.log("execute");
+
+const score = 200;
+
+// if (score > 100) {
+//   let power = "fly";
+//   console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+// const balance = 1000;
+
+// if (balance) console.log("test"),console.log("test2"),console.log("test3");
+
+// if (balance<500){
+//     console.log("less then 500");
+    
+// }else if(balance<750){
+//     console.log("less then 750");
+    
+// }else if (balance<900){
+//     console.log("less then 900");
+    
+// }else{
+//     console.log("less then 1100");
+    
+// }
+
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if(userLoggedIn && debitCard && 2==3){
+    console.log("allow to buy course");   
+}
+
+const loggedInFromGoogle = true;
+const loggedInFromEmail = false;
+
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("user is logged in");
+    
+}
