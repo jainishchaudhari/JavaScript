1a9ab77d3d1c7e298ffe7a222467f6032f5ff89f
Jainish <chaudharijainish9498@gmail.com>
2026-07-24 01:05:45 +0530

    for in

---
+++ 05_itrations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: "JavaScript",
+    cpp: "C++",
+    rb: "Rubby",
+    swift: "Swift by appple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} is for ${myObject[key]}`);
+    
+}
+
+const programing = ["js", "java", "rb", "cpp"];
+
+for (const key in programing) {
+    // console.log(programing[key]);
+       
+}
+
+// const map = new Map();
+
+// map.set('IN', "INDIA");
+// map.set('USA', "UNITED STATE OF AMERICA");
+// map.set('FR', "FRANCE");
+// map.set('IN', "INDIA");
+
+// for (const [key] in map) {
+//     console.log(map);
+// }
