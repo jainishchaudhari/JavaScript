497c7b3275c69c73580f1ad9e7bfb00d15b5ea39
Jainish <chaudharijainish9498@gmail.com>
2026-07-23 17:22:57 +0530

    while and do ... while

---
+++ 05_itrations/two.js
@@ -0,0 +1,25 @@
+//while
+
+let i = 0;
+while (i<=10) {
+    // console.log(`value is ${i}`);
+    i = i + 2;
+    
+}
+
+let myarr = ["flash", "ironman", "batman"];
+
+let arr = 0;
+
+while (arr<myarr.length) {
+    // console.log(`value is ${myarr[arr]}`);
+    arr = arr + 1;
+}
+
+
+let score = 1;
+
+do {
+    console.log(`score is ${score}`);
+    score++ 
+} while (score == 10);
