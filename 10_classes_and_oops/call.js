fe985fd3ee527a16fa2f852e0bdc9a1f822a93ac
Jainish <chaudharijainish9498@gmail.com>
2026-07-31 20:39:21 +0530

    call and this

---
+++ 10_classes_and_oops/call.js
@@ -0,0 +1,17 @@
+function setUserName (username) {
+    this.username = username;
+}
+
+
+function creatUser (username, email,password) {
+
+    setUserName.call(this, username)
+
+    this.email = email;
+    this.password = password;
+}
+
+
+const chai = new creatUser("chai", "chai@g.com",123)
+
+console.log(chai);
